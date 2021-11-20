f7fc725e3c2e3b7bdb8da26c8bbb9fd2dbc449a5
Novojit Das <38981630+novojitdas@users.noreply.github.com>
2021-11-21 00:44:25 +0600

    new added

---
+++ Codeforces/236A.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(){
+      int count=0;
+      string username;
+      cin >> username;
+      sort(username.begin(),username.end());
+      for(int i=0;i<username.size();i++)
+     {
+         if(username[i]!=username[i+1])
+         {
+               count++;
+         }
+     }
+      if(count%2==0)
+      {
+            cout <<"CHAT WITH HER!" << endl;
+      }
+      else
+      {
+            cout<<"IGNORE HIM!" << endl;
+      }
+
+      return 0;
+}
